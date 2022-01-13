49d63a9d545dfb6f4498313b9276abce54b88505
BeaTowanda <bgonzalez.remax@gmail.com>
2022-01-13 06:35:45 -0300

    ejercicio clase 11 de enero

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require ("express")
+let app = express ()
+const path = require("path")
+/*app.get('/', (req, res) => {
+    res.send('Holita');
+})*/
+app.use(express.static(path.resolve(__dirname, './public')))
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname,"./views/home.html"))})
+
+   
+app.listen (3030,()=>console.log("servidor Funcionando ") )
